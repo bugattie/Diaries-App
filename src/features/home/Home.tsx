a967fc11d68e67bf417ae3d3bdd5ba54a506de78
bugattie <m.mohsinbarkaati@gmail.com>
2021-11-03 10:22:02 +0500

    editor feature set up

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -2,9 +2,8 @@ import React, { FC } from "react";
 
 const Home: FC = () => {
   return (
-    <div className="two-cols">
-      <div className="left"></div>
-      <div className="right"></div>
+    <div>
+      <p>Welcome user!</p>
     </div>
   );
 };
